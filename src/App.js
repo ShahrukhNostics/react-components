b1b3fac46cc0c27bbb00fb748318a488f57489d9
MacBook Pro <sk902224@gmail.com>
2024-01-11 12:26:03 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ExternalReactComponent from "./components/ExternalReactComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <ExternalReactComponent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ExternalReactComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ExternalReactComponent() {
+  return (
+    <div className="App">
+      <h1>Hello React..! ExternalReactComponent</h1>
+    </div>
+  );
+}
+
+export default ExternalReactComponent;
